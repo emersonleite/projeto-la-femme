0f9218da6f9ac5f0067d0753e58da30f1b7a4417
Emerson Leite <emerson.bl@gmail.com>
2020-06-12 12:07:45 -0300

    202006121207

--- frontend/src/components/TheProductOne.vue
+++ frontend/src/components/TheProductOne.vue
@@ -8,10 +8,7 @@
       <input class="product__amount" type="text" name="amount" v-model="amount" />
       <span class="product__price">{{formatedPrice}}</span>
     </div>
-    <button
-      class="product__button--cart"
-      @click="add_product_to_cart({id:product.id,name:product.name,amount})"
-    >
+    <button class="product__button--cart" @click="add_product_to_cart(product_to_cart)">
       Adicionar ao
       <br />carrinho
     </button>
@@ -19,20 +16,40 @@
 </template>
  
  <script>
-import { mapMutations } from "vuex";
+import { mapMutations, mapState } from "vuex";
 export default {
   data() {
-    return { amount: 0 };
+    return {
+      amount: 0,
+      in_cart: false
+    };
   },
   props: ["product"],
   methods: {
     ...mapMutations(["ADD_PRODUCT_TO_CART"]),
     add_product_to_cart(product) {
       /* Verificando se a quantidade escolhida do produto é maior que zero */
-      if (product.amount > 0) this.ADD_PRODUCT_TO_CART(product);
+      if (product.amount > 0 && !window.localStorage[`in_cart${product.id}`]) {
+        this.ADD_PRODUCT_TO_CART(product);
+        window.localStorage[`in_cart${product.id}`] = true;
+        window.localStorage.cart = JSON.stringify(this.cart);
+      } else window.alert("Item no carrinho ou igual a zero");
     }
   },
+  created() {
+    /* this.cart_after_reload = JSON.parse(window.localStorage.cart);
+    this.ADD_PRODUCT_TO_CART(this.cart_after_reload); */
+  },
   computed: {
+    ...mapState(["cart"]),
+    product_to_cart() {
+      return {
+        id: this.product.id,
+        name: this.product.name,
+        amount: this.amount,
+        in_cart: this.in_cart
+      };
+    },
     formatedPrice() {
       return this.product.price.toLocaleString("pt-br", {
         style: "currency",
